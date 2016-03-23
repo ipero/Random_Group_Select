ccf935c1619fb9a9d19e620f2a38b10c9cf0a806
Roman H <ipero@live.com>
2016-03-23 17:40:46 -0500

    Working, but not stable

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,33 +1,71 @@
 var myApp = angular.module('myApp', []);
 var teamNum;
-var setButtonArray;
+var setButtonArray = [];
+var studentsArrayHolder = ["Enrique", "Tracey", "Carl", "Joette", "Jeremy", "Kenzie",
+"Aaron", "Evan", "Biz", "Brady", "Roman", "Thomas", "Miles", "Michelle", "Neil", "Hank", "Mike",
+"Mark", "Riley", "Matthew"];
+
 
 myApp.controller('IntroController', ['$scope', '$http', function($scope, $http){
-
+  $scope.groupHolder = [];
   $scope.employeeArray = [];
   $scope.teamArray = [];
+  $scope.students = [];
+  $scope.allStudents = studentsArrayHolder;
+  $scope.newArrayOfArray = [];
 
-  $scope.meow = function(){
-    console.log("Meow");
-  };
-  $scope.saveEmployee = function(value){
-    console.log(value);
-    $scope.employeeArray.push(value);
-    $scope.employee = {};
-    console.log($scope.employeeArray);
-  };
+
   $scope.getData = function(){
        $http.get("/students").then(function(response){
-         $scope.allStudents = response.data.students;
-         teamNum= response.data.students.length/2;
-         console.log(teamNum);
-         setButtonArray(teamNum);
+         //$scope.allStudents = response.data.students;
+
+         teamNum= Math.floor(studentsArrayHolder.length/2);
+         setButton(teamNum);
        });
+
   }
   $scope.getData();
-  function setButtonArray(value){
-    var counter = 1;
-    while ()
-  }
 
+  function setButton(value){
+    var counter = 1;
+    while (counter<=teamNum){
+      $scope.teamArray.push(counter);
+      counter++;
+    }
+  }
+  $scope.numGroups = function(number){
+
+    studentsArrayHolder = shuffleArray($scope.allStudents);
+
+    var peoplePerGroup = Math.ceil(studentsArrayHolder.length/number);
+    var counter = 1;
+    while(counter <= number){
+      var group = [];
+      for (var i=1; i<=peoplePerGroup; i++){
+        if($scope.allStudents[0]){
+          console.log("I'm here in ");
+          group.push($scope.allStudents.pop());
+        }
+      }
+      $scope.groupHolder.push(group);
+      counter++;
+    }
+    // for(var i=0; i<peoplePerGroup; i++){
+    //   for(var j=0; j<number; j++){
+    //     if($scope.allStudents[0]){
+    //       $scope.newArrayOfArray[j][i]=$scope.allStudents.pop();
+    //     }
+    //   }
+    // }
+    console.log($scope.newArrayOfArray);
+  }
+  function shuffleArray(array){
+    for (var i = array.length - 1; i>0; i--){
+      var j = Math.floor(Math.random()*(i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
 }]);
