e08b891810fcd632b367d38cda8a61708d577774
Roman H <ipero@live.com>
2016-03-23 11:48:27 -0500

    Little update

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
+var teamNum;
+var setButtonArray;
 
 myApp.controller('IntroController', ['$scope', '$http', function($scope, $http){
+
   $scope.employeeArray = [];
+  $scope.teamArray = [];
 
   $scope.meow = function(){
     console.log("Meow");
@@ -15,7 +19,15 @@ myApp.controller('IntroController', ['$scope', '$http', function($scope, $http){
   $scope.getData = function(){
        $http.get("/students").then(function(response){
          $scope.allStudents = response.data.students;
+         teamNum= response.data.students.length/2;
+         console.log(teamNum);
+         setButtonArray(teamNum);
        });
   }
   $scope.getData();
+  function setButtonArray(value){
+    var counter = 1;
+    while ()
+  }
+
 }]);
